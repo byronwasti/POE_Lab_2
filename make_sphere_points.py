e2398bcc28e25d2f35b86ef912bfe765505de3f7
byronwasti <byron.wasti@gmail.com>
2015-02-05 19:45:24 -0500

    Making testing points which emulate what the Arduino will output

--- make_sphere_points.py
+++ make_sphere_points.py
@@ -1,6 +1,6 @@
 d = 10;
 f = open('points.txt','w')
-for i in range(0,361,5):
-    for j in range(0,361,5):
+for i in range(0,361,10):
+    for j in range(0,361,10):
         f.write( str(d)+','+str(i)+','+str(j)+'\n')
 
