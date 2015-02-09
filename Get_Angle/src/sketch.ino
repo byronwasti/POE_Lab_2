3a05a884b39a2df9b2c28d70f13f881b2f279968
byronwasti <byron.wasti@gmail.com>
2015-02-09 11:54:55 -0500

    Fixed up so that we can find the angle needed for start/end times for the servos

--- Get_Angle/src/sketch.ino
+++ Get_Angle/src/sketch.ino
@@ -1,6 +1,7 @@
 #include <Servo.h>
 Servo ser;
 int angle = 90;
+int tmp;
 
 void setup()
 {
@@ -10,8 +11,16 @@ void setup()
 
 void loop()
 {
-    if (Serial.available() > 0) angle = Serial.read();
+    if (Serial.available() > 0){
+        tmp = Serial.read();
+        Serial.print(tmp);
+        if ( tmp > 49 ) angle += 1;
+        else angle -= 1;
+        Serial.print(" : ");
+        Serial.println(angle);
+    }
 
-    ser.write(angle);
+    //ser.write(angle);
     delay(15);
+    //Serial.println(angle);
 }
